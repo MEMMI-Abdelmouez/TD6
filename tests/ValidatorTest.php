0561c9e2c7f14ce7eebcd51ead9cbf302416e766
MEMMI-Abdelmouez <abdelmouez.memmi@gmail.com>
2024-04-16 13:58:53 +0200

    Commit validatorTest 2

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -3,7 +3,6 @@ use PHPUnit\Framework\TestCase;
 class ValidatorTest extends TestCase{
     public function test_Nbr_char(){
         $instance = new \TestUnitaire\Validator();
-        $resultat = $instance->Nbr_char("a");
-        $this->assertFalse($resultat);
+        self::assertFalse($instance->Nbr_char("a"));
     }
 }
